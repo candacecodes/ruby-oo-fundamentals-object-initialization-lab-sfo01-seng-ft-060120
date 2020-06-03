92965466b7ffb28c3a8589e847d312c852947735
eager-class-5814 <candacehchoe@gmail.com>
2020-06-03 16:33:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,21 @@
 class Dog 
+<<<<<<< HEAD
   def initialize(dog_name, dog_breed="Mutt")
     @name=dog_name
     @breed=dog_breed
   end 
 end
+=======
+def initialize (breed)
+  @breed = "Mutt"
+end 
+
+def initialize(name)
+  @name = name
+end
+
+def breed=(breed)
+  @breed = breed
+end 
+end 
+>>>>>>> b710136a2ea8dd782aa2ee3a5e537f400a88f093
